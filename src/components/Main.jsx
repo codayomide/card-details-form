5522b10103a6e14b72ef21aad497a7773eda2e55
codayomide <smokayomide@gmail.com>
2023-07-10 17:26:34 +0100

    Done with form validation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,6 +14,8 @@ const Main = () => {
         cvc: ''
     });
 
+    const [errors, setErrors] = useState({})
+
 
     const handleInputChange = (event) => {
         const { name, value } = event.target;
@@ -23,6 +25,48 @@ const Main = () => {
             [name]: value,
         }));
     };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const validationErrors = {};
+
+        if (!formData.name) {
+            validationErrors.name = `Can't be blank`;
+        }
+
+        if (!formData.cardNumber) {
+            validationErrors.cardNumber = `Can't be blank`;
+        } else if (!/[0-9]*/.test(formData.cardNumber)) {
+            validationErrors.cardNumber = 'Wrong format, numbers only';
+        } else if (formData.cardNumber.length < 16) {
+            validationErrors.cardNumber = 'Too short';
+        }
+
+        if (!formData.month) {
+            validationErrors.month = `Can't be blank`;
+        }
+
+        if (!formData.year) {
+            validationErrors.year = `Can't be blank`;
+        }
+
+        if (!formData.month || !formData.year) {
+            validationErrors.date = `Can't be blank`;
+        }
+
+        if (!formData.cvc) {
+            validationErrors.cvc = `Can't be blank`
+        } else if (!/[0-9]*/.test(formData.cvc)) {
+            validationErrors.cardNumber = 'Wrong format, numbers only'
+        }
+
+        setErrors(validationErrors);
+
+        if (Object.keys(validationErrors).length === 0) {
+            setValidate(true);
+        }
+    }
     
     return (
         <main id="main">
@@ -34,6 +78,8 @@ const Main = () => {
 					: <FormInitialState 
                         onInputChange={handleInputChange}
                         formData={formData}
+                        onSubmit={handleSubmit}
+                        errors={errors}
                     />
 				}
             </section>
--- src/components/form/FormInitialState.jsx
+++ src/components/form/FormInitialState.jsx
@@ -1,14 +1,10 @@
 import { useState } from "react";
 
-const FormInitialState = ({ onInputChange, formData }) => {
+const FormInitialState = ({ onInputChange, formData, onSubmit, errors }) => {
     const [isPending, setIsPending] = useState(false);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    }
-
     return (
-        <form id="form" onSubmit={handleSubmit}>
+        <form id="form" onSubmit={onSubmit}>
             <div className="input-container">
                 <label htmlFor="Name">Cardholder Name</label>
                 <input
@@ -17,10 +13,11 @@ const FormInitialState = ({ onInputChange, formData }) => {
                     id="name"
                     name="name"
                     value={formData.name}
-                    required
                     onChange={onInputChange}
                     maxLength={64}
+                    style={errors.name ? {borderColor: 'hsl(0, 100%, 66%)'} : {borderColor: 'hsl(279, 6%, 55%)'}}
                 />
+                {errors.name && <p>{errors.name}</p>}
             </div>
 
             <div className="input-container">
@@ -30,10 +27,11 @@ const FormInitialState = ({ onInputChange, formData }) => {
                     id="number"
                     name="cardNumber"
                     value={formData.cardNumber}
-                    required
                     onChange={onInputChange}
-                    maxLength={19}
+                    maxLength={16}
+                    style={errors.cardNumber ? {borderColor: 'hsl(0, 100%, 66%)'} : {borderColor: 'hsl(279, 6%, 55%)'}}
                 />
+                {errors.cardNumber && <p>{errors.cardNumber}</p>}
             </div>
 
             <div className="input-container__type-2">
@@ -42,26 +40,31 @@ const FormInitialState = ({ onInputChange, formData }) => {
                     <div>
                         <div id="date-input-container">
                             <input
-                                type="text" 
+                                type="number" 
                                 placeholder="MM"
                                 id="month"
                                 name="month"
                                 value={formData.month}
-                                required
                                 onChange={onInputChange}
+                                min={0}
+                                max={12}
                                 maxLength={2}
+                                style={errors.month ? {borderColor: 'hsl(0, 100%, 66%)'} : {borderColor: 'hsl(279, 6%, 55%)'}}
                             />
                             <input  
-                                type="text"
+                                type="number"
                                 placeholder="YY"
                                 id="year"
                                 name="year"
                                 value={formData.year}
-                                required
                                 onChange={onInputChange}
+                                min={0}
+                                max={99}
                                 maxLength={2}
+                                style={errors.year ? {borderColor: 'hsl(0, 100%, 66%)'} : {borderColor: 'hsl(279, 6%, 55%)'}}
                             />
                         </div>
+                        {errors.date && <p>{errors.date}</p>}
                     </div>
                 </div>
                 
@@ -72,10 +75,11 @@ const FormInitialState = ({ onInputChange, formData }) => {
                         placeholder="e.g. 123"
                         name="cvc"
                         value={formData.cvc}
-                        required
                         onChange={onInputChange}
                         maxLength={3}
+                        style={errors.cvc ? {borderColor: 'hsl(0, 100%, 66%)'} : {borderColor: 'hsl(279, 6%, 55%)'}}
                     />
+                    {errors.cvc && <p>{errors.cvc}</p>}
                 </div>
             </div>
 
